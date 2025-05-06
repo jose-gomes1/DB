1a5e18e1e805784b4e83a789a3a86dd304d6802e
jose-gomes1 <145759337+jose-gomes1@users.noreply.github.com>
2025-05-06 09:08:19 +0000

    Update bash2.sh

--- bash2.sh
+++ bash2.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 date=${date '+_%m_%d_%H:%M'}
+base = "$1"
 
 if [ -z "$1" ]; then
     echo "Usage: $0 <file>"
@@ -8,7 +9,6 @@ if [ -z "$1" ]; then
 else
     cp "$1" "${base}$date.bck"
 fi
-base = "${1%.*}"
 echo "Created $1 as $2"
 
 # TPC: Fazer o clone criar como <filename>_Data_Hora.bck 
