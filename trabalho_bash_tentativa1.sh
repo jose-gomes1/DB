1ff36951e2aba1f5414b9a0eb6fa025eb326e0b7
jose-gomes1 <josemgomes2017@gmail.com>
2025-06-04 09:24:48 +0000

    Update da data

--- trabalho_bash_tentativa1.sh
+++ trabalho_bash_tentativa1.sh
@@ -30,4 +30,10 @@ BACKUP_FILE="${DB_NAME}${TIMESTAMP}.sql"
 # Executa o dump dentro do container e redireciona a saída para um ficheiro no host
 echo "A fazer backup da base de dados '$DB_NAME' no container '$CONTAINER_NAME' para '$BACKUP_FILE'..."
 docker exec "$CONTAINER_NAME" mysqldump "$DB_NAME" -u$USER -p$PASS > "$DEST_FILE"/"$BACKUP_FILE"
-echo "Backup criado: $BACKUP_FILE"
+
+if [ $? -eq 0 ]; then
+    echo "Backup criado: $BACKUP_FILE"
+else
+    echo "Erro a criar o backup"
+    exit 2
+fi
